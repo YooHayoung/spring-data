2ac97c1b7b369aa8131feb7afbe9d8e95f87f10e
YooHayoung <yoo971202@naver.com>
2022-12-10 12:49:34 +0900

    init

---
+++ src/test/java/com/example/dbaccess/DbAccessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dbaccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbAccessApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
